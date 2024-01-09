7d6f9d3490c350ab333596c04f8e29094b9a4311
Robson <robsonribeirorra@gmail.com>
2024-01-09 09:56:09 -0300

    UserController created

---
+++ src/main/java/com/picpayChallenge/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.picpayChallenge.controllers;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.picpayChallenge.dtos.UserDto;
+import com.picpayChallenge.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping(value = "/user")
+@CrossOrigin
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<UserDto> getUsers() {
+        return userService.getUsers();
+    }
+
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto user) {
+        return userService.createUser(user);
+    }
+}
