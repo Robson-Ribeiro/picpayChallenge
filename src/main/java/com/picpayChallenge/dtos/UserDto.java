1022f04071bade0c8964f2668626b0ed9e3bf30e
Robson <robsonribeirorra@gmail.com>
2024-01-09 07:52:30 -0300

    UserEntity Created

---
+++ src/main/java/com/picpayChallenge/dtos/UserDto.java
@@ -0,0 +1,5 @@
+package com.picpayChallenge.dtos;
+
+public class UserDto {
+    
+}
---
+++ src/main/java/com/picpayChallenge/entities/UserEntity.java
@@ -0,0 +1,63 @@
+package com.picpayChallenge.entities;
+
+import org.springframework.beans.BeanUtils;
+
+import com.picpayChallenge.dtos.UserDto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "Users")
+public class UserEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false, unique = true)
+    private String document;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private double balance;
+
+    @Column(nullable = false)
+    private String userType;
+
+    public UserEntity(String name, String surname, String document, String email, String password, double balance, String userType) {
+        this.name = name;
+        this.surname = surname;
+        this.document = document;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.userType = userType;
+    }
+
+    public UserEntity(UserDto user) {
+        BeanUtils.copyProperties(user, this);
+    }
+
+}
