4495274a88713e90fdc18c6371e1731708d2da9b
Robson <robsonribeirorra@gmail.com>
2024-01-09 09:24:56 -0300

    TransactionDto created

--- src/main/java/com/picpayChallenge/dtos/TransactionDto.java
+++ src/main/java/com/picpayChallenge/dtos/TransactionDto.java
@@ -1,5 +1,28 @@
 package com.picpayChallenge.dtos;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
 public class TransactionDto {
+
+    private Long id;
+
+    private Long receiverId;
+
+    private Long payerId;
+
+    private double value;
+
+    public TransactionDto(Long receiverId, Long payerId, double value) {
+        this.receiverId = receiverId;
+        this.payerId = payerId;
+        this.value = value;
+    }
 
 }
