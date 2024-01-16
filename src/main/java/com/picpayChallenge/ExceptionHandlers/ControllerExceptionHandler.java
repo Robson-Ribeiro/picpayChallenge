24a4270fd0badec28a9d78621616fd3bf647eadd
Robson <robsonribeirorra@gmail.com>
2024-01-16 00:54:54 -0300

    Trying to fix the ExceptionHandler

--- src/main/java/com/picpayChallenge/ExceptionHandlers/ControllerExceptionHandler.java
+++ src/main/java/com/picpayChallenge/ExceptionHandlers/ControllerExceptionHandler.java
@@ -1,15 +1,27 @@
 package com.picpayChallenge.ExceptionHandlers;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.picpayChallenge.dtos.ExceptionDto;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
-public class ControllerExceptionHandler {
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ExceptionDto userRepeatsUniqueCredential(DataIntegrityViolationException exception) {
-        return new ExceptionDto("Your credential are already in use!", "400");
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity generic(Exception exception) {
+        System.out.println(11111);
+        return ResponseEntity.badRequest().body(new ExceptionDto(exception.getMessage(), "500"));
+    }
+    
+    @ExceptionHandler(value = DataIntegrityViolationException.class)
+    public ResponseEntity userRepeatsUniqueCredential(DataIntegrityViolationException exception) {
+        return ResponseEntity.badRequest().body(new ExceptionDto("Your credential are already in use!", "400"));
     }
 }
